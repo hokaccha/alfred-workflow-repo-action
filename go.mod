688f56bb960e017844a1a467d236b8cb0832e741
Kazuhito Hokamura <k.hokamura@gmail.com>
2021-08-19 00:47:53 +0900

    First implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/hokaccha/alfred-workflow-repo-action
 
 go 1.16
+
+require github.com/deanishe/awgo v0.29.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"path/filepath"
+	"strings"
+
+	aw "github.com/deanishe/awgo"
+)
+
+var wf *aw.Workflow
+
+func init() {
+	wf = aw.New()
+}
+
+func run() {
+	basedir := os.Args[1]
+	query := os.Args[2]
+	dirs, err := filepath.Glob(basedir + "/*/*/*")
+	if err != nil {
+		wf.FatalError(err)
+	}
+	for _, dir := range dirs {
+		content := strings.Replace(dir, basedir+"/", "", 1)
+		arr := strings.Split(content, "/")
+		host, owner, repo := arr[0], arr[1], arr[2]
+		wf.NewItem(content).Arg(basedir, host, owner, repo).Valid(true)
+	}
+	wf.Filter(query)
+	wf.SendFeedback()
+}
 
 func main() {
-	fmt.Println("hello")
+	wf.Run(run)
 }
